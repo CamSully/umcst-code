9b5d9ac69f44b7280cb0da5b5c88910ee7900640
Cameron Sullivan <arrowc11@gmail.com>
2017-11-28 19:28:52 -0500

    Files

---
+++ joinAD.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Credit: this script was originally written by user bjh7242 on Github.
+# It was first used by the NECCDC black team to set up the competition network.
+# Link: github.com/bjh7242/NECCDC-2017-Configs/blob/master/ansible/corp/playbooks/scripts/join.sh
+# I modified the script to work on UMCST's network.
+
+# Prerequisite: configure /etc/resolv.conf - set AD DC in nameserver and domain in search.
+# Run this script with sudo!
+
+# Install dependencies (-y for auto 'yes').
+apt-get install windbind samba smbclient krb5-user libpam-winbind libnss-winbind -y
+
+# Modify smb.conf: configure for auth to our domain.
+cat <<EOF > /etc/samba/smb.conf
+[global]
+    workgroup = UMCSTLAB
+    security = ads
+    realm = UMCSTLAB.NET
+    domain master = no
+    local master = no
+    preferred master = no
+    idmap backend = tdb
+    idmap uid = 10000-99999
+    idmap gid = 10000-99999
+    winbind enum users = yes
+    winbind enum groups = yes
+    winbind use default domain = yes
+    winbind nested groups = yes
+    winbind refresh tickets = yes
+    template homedir = /home/%D/%U
+    template shell = /bin/bash
+    client use spnego = yes
+    client ntlmv2 auth = yes
+    encrypt passwords = yes
+    restrict anonymous = 2
+    log file = /var/log/samba/log.%m
+    max log size = 50
+    winbind offline logon = true
+EOF
+service smbd restart
+service winbind restart
+# Join the server to the domain (will query for pass)
+net ads join -U Administrator
+
+# Update pam to enable Windows authentication. 
+pam-auth-update
+# Edit config files for authentication.
+sed -i 's/passwd.*/passwd:         compat winbind/g' /etc/nsswitch.conf
+sed -i 's/group.*/group:          compat winbind/g' /etc/nsswitch.conf
+sed -i 's/shadow.*/shadow:         compat winbind/g' /etc/nsswitch.conf
+echo "session required			pam_mkhomedir.so skel=/etc/skel umask=0022" >> /etc/pam.d/common-account
+service smbd restart
+service winbind restart
+echo 'Done!'
---
+++ ldapWorker.py
@@ -0,0 +1,37 @@
+# To install, use 'apt-get install python-ldap'
+import ldap
+
+# Initialize variables.
+# Note that the password should be replaced with the real password.
+ldapServer = "10.50.1.10"
+user = "dude@berrysecure.net"
+passwd = "###PASS###"
+
+
+def ldapAuthen(ldapServer, user, passwd):
+    try:
+        # Connect to the ldap server via port 389
+        l = ldap.open(host=ldapServer, port=389);
+        
+        # Set some options to allow it to authenticate.
+        l.protocol_version = 3
+        l.set_option(ldap.OPT_REFERRALS, 0)
+
+        # Authenticate to the AD.
+        l.simple_bind_s(user, passwd)
+        print ("LDAP object bound")
+
+        # Exit after authentication
+        l.unbind_s()
+        print ("Unbound")
+
+        # Return an array indicating successful authentication.
+        return([1, ""])
+
+    # If an exception is created, print a descriptive message and return an array indicating failure.
+    except ldap.LDAPError, e:
+        print "Exception created: " + e[0]["desc"]
+        return([0, e[0]["desc"]])
+
+ldapAuthen(ldapServer, user, passwd)
+        
